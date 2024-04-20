6e763e31bbff184219d33e775d98cddb5ce82cb9
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2024-04-20 23:03:43 +0700

    feat: integrate payment with ZaloPay

--- README.md
+++ README.md
@@ -1,34 +1,94 @@
-# Updates!
+# Costume Rental Service
 
-This new version contains an integration with redux-toolkit instead of redux and it's witch Typescript :)
+Link deploy: https://clothes-rent-app.vercel.app
 
-# Next.js Ecommerce
+# Integrate Payment with ZaloPay
 
-This repo contains a work in progress Ecommerce responsive made with Next.js, Redux, Redux-persist, Hooks, SCSS and BEM. If you like it please give it a star :)
-## Design
+## Config
 
-[This](https://www.xdguru.com/free-xd-ecommerce-ui-kit-by-iceo/) is the design of the project.
+1. Update `next.config.js` file with your [Merchant information](https://docs.zalopay.vn/v2/start/):
 
-## Available pages
+```sh
+ZLP_MERCHANT_APP_ID="your_app_id_here"
+ZLP_MERCHANT_KEY1="your_key1_here"
+ZLP_MERCHANT_KEY2="your_key2_here"
+ZLP_MERCHANT_CALLBACK_URL="zalopay_merchant_endpoint_here"
+```
 
-- Home page: /
-- Products page: /products
-- Product single page: /product/1
-- Cart page: /cart
-- Login page: /login
-- Register page: /register
-- 404 page: /page-not-found
+Or you can use this [testing merchant account](https://docs.zalopay.vn/v2/start/#A-V).
 
-## Screenshots
+```sh
+ZLP_MERCHANT_APP_ID: '2554',
+ZLP_MERCHANT_KEY1: 'sdngKKJmqEMzvh5QQcdD2A9XBSKUNaYn',
+ZLP_MERCHANT_KEY2: 'trMrHtvjo6myautxDUiAcYsVtaeQ8nhf',
+ZLP_MERCHANT_ENDPOINT: 'https://sb-openapi.zalopay.vn/v2/',
+ZLP_MERCHANT_CALLBACK_URL:'ngrok http 3000' + /api/callback
+```
 
-![Next Ecommerce screenshot](https://lucaspulliese.com/wp-content/uploads/2020/09/ecommerce-1.jpg)
+## Install ngrok
 
-![Next Ecommerce screenshot](https://lucaspulliese.com/wp-content/uploads/2020/09/ecommerce-2.jpg)
+MacOS:
 
-## Next TO-DO
+```sh
+brew cask install ngrok
+```
 
-- [X] Checkout page
-- [ ] Add Facebook login
-- [ ] Add Google login
-- [ ] Add Proptypes on components
-- [ ] Use CSS variables instead of static colors
+Register ngrok account: https://dashboard.ngrok.com/
+
+You can find your_auth_token after register ngrok in https://dashboard.ngrok.com/get-started/setup/macos
+
+```sh
+ngrok config add-authtoken <your_auth_token>
+```
+
+## Run Source
+
+### Expose ngrok callback
+
+```
+ngrok http 3000
+```
+
+![Ngrok](/assets/images/ngrok.png)
+
+Add callback url to next.config.js, example:
+
+```
+ZLP_MERCHANT_CALLBACK_URL= https://3a7a-2a09-bac5-d46a-16c8-00-245-53.ngrok-free.app/api/callback
+```
+
+Install dependencies
+
+```
+yarn install
+```
+
+Run dev
+
+```
+yarn start
+```
+
+Your app is running at: http://localhost:3000
+
+Please register and login with your information
+
+# Usage
+
+1. Install ZaloPay QC App [https://docs.zalopay.vn/v2/start/#A](https://docs.zalopay.vn/v2/start/#A).
+
+![ZaloPay QC](/assets/images/ZaloPay%20QC.jpeg)
+
+Remember to trust the app, in IPhone you come to Settings -> General -> VPN & Device Management -> TRUST ZION JOIN STOCK COMPANY
+
+2. Register and deposit money into wallet (not worry, it is free).
+
+3. Scan QR Code And Test with ZaloPay QC App.
+
+![Payment_Procees](assets/images/Payment_Procees.png)
+
+![Payment_QR](assets/images/Payment_QR.png)
+
+![Payment_ZaloPay](assets/images/Payment_ZaloPay.jpeg)
+
+![Payment_Success](assets/images/Payment_Success.png)
---
+++ pages/cart/payment.tsx
@@ -0,0 +1,130 @@
+import { message, QRCode, Spin, Typography } from 'antd';
+import React, { useEffect, useState } from 'react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import axios from 'axios';
+
+const { Title, Paragraph, Text } = Typography;
+
+const Checkout = () => {
+  const [qrCode, setQrCode] = useState();
+  const [appTransId, setAppTransId] = useState();
+  const [secondsToGo, setSecondsToGo] = useState(60);
+  const router = useRouter();
+
+  useEffect(() => {
+    async function createOrder() {
+      // create ZLP order
+      const res = await axios.post('/api/create_order');
+      setQrCode(res.data.url);
+      setAppTransId(res.data.appTransID);
+    }
+
+    createOrder();
+  }, []);
+
+  useEffect(() => {
+    let timer = setInterval(() => {
+      if (secondsToGo > 0) {
+        setSecondsToGo(secondsToGo - 1);
+      }
+      if (secondsToGo === 0) {
+        clearInterval(timer);
+        message
+          .error('Out of time, please try again!')
+          .then(() => router.push('/cart/checkout'));
+      }
+    }, 1000); // interval check after 1s, simulate a countdown timer
+    return () => {
+      clearInterval(timer);
+    };
+  });
+
+  useEffect(() => {
+    let checkPaymentStatus = setInterval(async () => {
+      // interval query order ZLP status
+      const res = await axios.post('/api/query_status', {
+        appTransId: appTransId,
+      });
+      const returnCode = res.data.return_code;
+      if (returnCode === 1) {
+        clearInterval(checkPaymentStatus);
+        router.push('/status/success');
+      }
+    }, 1000);
+    return () => {
+      clearInterval(checkPaymentStatus);
+    };
+  });
+
+  return (
+    <div>
+      <Head>
+        <title>My Shopping Cart </title>
+      </Head>
+      <div className="container xl:max-w-screen-xl mx-auto py-12 px-6 flex flex-col justify-center items-center text-center">
+        <div className="payment-page">
+          <div id="payment-modal">
+            <div
+              id="qr-code"
+              className="flex flex-col justify-center items-center text-center">
+              <QRCode value={qrCode ?? ''} />
+            </div>
+            <br />
+            <Typography>
+              <Title type="success" level={4}>
+                Waiting for payment ...
+              </Title>
+              <Paragraph>
+                <Spin /> Time to scan QR codes for payment{' '}
+                <Text type="danger">{secondsToGo}</Text> seconds
+              </Paragraph>
+              <br />
+              <Title type="secondary" level={4}>
+                Pay with{' '}
+                <img
+                  src="/images/logozlp.png"
+                  id="zlp-logo-image"
+                  className="checkout-image"
+                  alt=""
+                />{' '}
+                by QR code
+              </Title>
+              <br />
+              <div id="payment-steps">
+                <Text strong>Payment Guide: </Text>
+                <br />
+                <br />
+                <ul>
+                  <li>
+                    <p>
+                      Step 1: Open <Text strong>ZaloPay</Text> app
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Step 2: Select <Text strong>"Thanh Toán"</Text>{' '}
+                      <img
+                        src="/images/qr-scan-zlp.png"
+                        className="checkout-image"
+                        alt=""
+                      />{' '}
+                      and scan QR code
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Step 3: <Text strong>Confirm payment</Text> on ZaloPay app
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            </Typography>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;
